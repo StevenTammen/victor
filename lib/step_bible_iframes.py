b12a4cef94c9b876e920eba7f904c2d06ede99b7
Steven Tammen <stevenwtammen@gmail.com>
2026-03-15 14:00:54 -0400

    Tweak how STEP Bible iframes get processed on slides

--- lib/step_bible_iframes.py
+++ lib/step_bible_iframes.py
@@ -38,22 +38,22 @@ def build_nt_iframe(match_obj):
   iframe = ''
   
   if(iframe_type == "esv"):
-    iframe = f'<iframe src="https://www.STEPBible.org/?q=version=ESV|reference={passage}&options=VN" width="100%" height="80%"></iframe>'
+    iframe = f'<iframe src="https://www.STEPBible.org/?q=version=ESV|reference={passage}&options=VN" width="100%" height="{height}"></iframe>'
   elif(iframe_type == "nasb"):
-    iframe = f'<iframe src="https://www.STEPBible.org/?q=version=NASB_th|reference={passage}&options=VN" width="100%" height="80%"></iframe>'
+    iframe = f'<iframe src="https://www.STEPBible.org/?q=version=NASB_th|reference={passage}&options=VN" width="100%" height="{height}"></iframe>'
   elif(iframe_type == "comparison"):
-    iframe = f'<iframe src="https://www.STEPBible.org/?q=version=ESV|version=NASB_th|version=NIV|version=HCSB|reference={passage}&options=VN" width="100%" height="80%"></iframe>'
+    iframe = f'<iframe src="https://www.STEPBible.org/?q=version=ESV|version=NASB_th|version=NIV|version=HCSB|reference={passage}&options=VN" width="100%" height="{height}"></iframe>'
   elif(iframe_type == "greek"):
-    iframe = f'<iframe src="https://www.STEPBible.org/?q=version=THGNT|reference={passage}&options=VGN" width="100%" height="80%"></iframe>'
+    iframe = f'<iframe src="https://www.STEPBible.org/?q=version=THGNT|reference={passage}&options=VGN" width="100%" height="{height}"></iframe>'
   elif(iframe_type == "greek-i"):
-    iframe = f'<iframe src="https://www.STEPBible.org/?q=version=THGNT|version=NASB_th|reference={passage}&options=VGN" width="100%" height="80%"></iframe>'
+    iframe = f'<iframe src="https://www.STEPBible.org/?q=version=THGNT|version=NASB_th|reference={passage}&options=VGN" width="100%" height="{height}"></iframe>'
   else:
     raise Exception(f"The iframe type '{iframe_type}' is not valid")
   
   # Let shorter iframes take up less space if specified as such. 600px and 800px 
   # iframes we will always be splitting onto different slides (80% ~ 512px ish)
-  if(height == "300px" or height == "400px"):
-    iframe = iframe.replace("80%", height)
+  # if(height == "300px" or height == "400px"):
+  #   iframe = iframe.replace("80%", height)
 
   return iframe
 
@@ -73,6 +73,7 @@ def build_ot_iframe(match_obj):
   parameters = match_obj.group(1)
   iframe_type = (type_re_pattern.search(parameters)).group(1)
   passage = (passage_re_pattern.search(parameters)).group(1)
+  height = (height_re_pattern.search(parameters)).group(1)
 
   # Get passage ready to get plugged into the URL 
   # for the API hit
@@ -85,18 +86,18 @@ def build_ot_iframe(match_obj):
   )
 
   if(iframe_type.lower() == "esv"):
-    return f'<iframe src="https://www.STEPBible.org/?q=version=ESV|reference={passage}&options=VN" width="100%" height="80%"></iframe>'
+    return f'<iframe src="https://www.STEPBible.org/?q=version=ESV|reference={passage}&options=VN" width="100%" height="{height}"></iframe>'
   elif(iframe_type.lower() == "nasb"):
-    return f'<iframe src="https://www.STEPBible.org/?q=version=NASB_th|reference={passage}&options=VN" width="100%" height="80%"></iframe>'
+    return f'<iframe src="https://www.STEPBible.org/?q=version=NASB_th|reference={passage}&options=VN" width="100%" height="{height}"></iframe>'
   elif(iframe_type.lower() == "comparison"):
-    return f'<iframe src="https://www.STEPBible.org/?q=version=ESV|version=NASB_th|version=NIV|version=HCSB|reference={passage}&options=VN" width="100%" height="80%"></iframe>'
+    return f'<iframe src="https://www.STEPBible.org/?q=version=ESV|version=NASB_th|version=NIV|version=HCSB|reference={passage}&options=VN" width="100%" height="{height}"></iframe>'
   elif(iframe_type.lower() == "hebrew"):
-    return f'<iframe src="https://www.STEPBible.org/?q=version=THOT|reference={passage}&options=VUN" width="100%" height="80%"></iframe>'
+    return f'<iframe src="https://www.STEPBible.org/?q=version=THOT|reference={passage}&options=VUN" width="100%" height="{height}"></iframe>'
   elif(iframe_type.lower() == "hebrew-i"):
-    return f'<iframe src="https://www.STEPBible.org/?q=version=THOT|version=NASB_th|reference={passage}&options=VUN" width="100%" height="80%"></iframe>'
+    return f'<iframe src="https://www.STEPBible.org/?q=version=THOT|version=NASB_th|reference={passage}&options=VUN" width="100%" height="{height}"></iframe>'
   elif(iframe_type.lower() == "septuagint"):
-    return f'<iframe src="https://www.STEPBible.org/?q=version=THOT|version=LXX|reference={passage}&options=VGN" width="100%" height="80%"></iframe>'
+    return f'<iframe src="https://www.STEPBible.org/?q=version=THOT|version=LXX|reference={passage}&options=VGN" width="100%" height="{height}"></iframe>'
   elif(iframe_type.lower() == "septuagint-i"):
-    return f'<iframe src="https://www.STEPBible.org/?q=version=THOT|version=LXX|version=NASB_th|reference={passage}&options=VGN" width="100%" height="80%"></iframe>'
+    return f'<iframe src="https://www.STEPBible.org/?q=version=THOT|version=LXX|version=NASB_th|reference={passage}&options=VGN" width="100%" height="{height}"></iframe>'
   else:
     raise Exception(f"The iframe type '{iframe_type}' is not valid")
